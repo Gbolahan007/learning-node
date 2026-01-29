959073cb4ab3128cbb1c69be2e33057786d5fffb
Lawal Omogbolahan <lawalomogbolahan08@gmail.com>
2026-01-29 17:01:31 +0100

    learning aggregates

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,56 +1,114 @@
 const Tour = require('../models/tourModel');
+const apiFeatures = require('../utils/apiFeatures');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+  next();
+};
 // GET all tours
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-
-    //BUILD QUERY
+    let query = Tour.find();
     const queryObj = { ...req.query };
 
-    //  FILTERING
-    const excludeFields = ['page', 'limit', 'sort', 'field'];
-    excludeFields.forEach((el) => delete queryObj[el]);
+    query = apiFeatures.filter(query, queryObj);
+    query = apiFeatures.sort(query, req.query);
+    query = apiFeatures.limitFields(query, req.query);
+    query = await apiFeatures.paginate(query, req.query, Tour);
 
-    // ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
-
-    console.log(JSON.parse(queryStr));
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort().split(',').join(' ');
-
-      console.log(sortBy);
-      query = query.sort(req.query.sort);
-    }
-    //EXECUTE QUERY
     const tours = await query;
 
-    // {difficulty:'easy', duration:{$gte:5}}
-    //    const query = Tour.find(queryObj)
-    //   .where('duration'),
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       result: tours.length,
-      data: {
-        tours,
-      },
+      data: { tours },
     });
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
+
+// exports.getAllTours = async (req, res) => {
+//   try {
+//     console.log(req.query);
+
+//     //BUILD QUERY
+//     const queryObj = { ...req.query };
+
+//     //  FILTERING
+//     const excludeFields = ['page', 'limit', 'sort', 'fields'];
+//     excludeFields.forEach((el) => delete queryObj[el]);
+
+//     // ADVANCED FILTERING
+//     let queryStr = JSON.stringify(queryObj);
+//     queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
+
+//     let query = Tour.find(JSON.parse(queryStr));
+
+//     // SORTING
+//     if (req.query.sort) {
+//       const sortBy = req.query.sort.split(',').join(' ');
+
+//       query = query.sort(sortBy);
+//     } else {
+//       query = query.sort('-createdAt');
+//     }
+
+//     //FIELD LIMITING
+//     if (req.query.fields) {
+//       const fields = req.query.fields.split(',').join(' ');
+//       query = query.select(fields);
+//     } else {
+//       query = query.select('-__v');
+//     }
+
+//     //PAGINATION
+//     const page = req.query.page * 1 || 1;
+//     const limit = req.query.limit * 1 || 100;
+
+//     const skip = (page - 1) * limit;
+//     //?page=2&limit=10
+//     query = query.skip(skip).limit(limit);
+
+//     if (req.query.page) {
+//       const numTours = await Tour.countDocuments();
+
+//       if (skip >= numTours) {
+//         throw new Error('this page does not exist');
+//       }
+//     }
+
+//     //EXECUTE QUERY
+//     const tours = await query;
+
+//     // {difficulty:'easy', duration:{$gte:5}}
+//     //    const query = Tour.find(queryObj)
+//     //   .where('duration'),
+//     //   .equals(5)
+//     //   .where('difficulty')
+//     //   .equals('easy');
+
+//     //SEND RESPONSE
+//     res.status(200).json({
+//       status: 'success',
+//       result: tours.length,
+//       data: {
+//         tours,
+//       },
+//     });
+//   } catch (err) {
+//     res.status(400).json({
+//       status: 'fail',
+//       message: err,
+//     });
+//   }
+// };
 
 // GET a single tour by ID
 exports.getTour = async (req, res) => {
@@ -122,6 +180,39 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: 'success',
       message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          sumTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          mixPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
     });
   }
 };
---
+++ utils/apiFeatures.js
@@ -0,0 +1,36 @@
+exports.filter = (query, queryObj) => {
+  const excludeFields = ['page', 'limit', 'sort', 'fields'];
+  excludeFields.forEach((el) => delete queryObj[el]);
+
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (m) => `$${m}`);
+
+  return query.find(JSON.parse(queryStr));
+};
+
+exports.sort = (query, reqQuery) => {
+  if (reqQuery.sort) {
+    return query.sort(reqQuery.sort.split(',').join(' '));
+  }
+  return query.sort('-createdAt');
+};
+
+exports.limitFields = (query, reqQuery) => {
+  if (reqQuery.fields) {
+    return query.select(reqQuery.fields.split(',').join(' '));
+  }
+  return query.select('-__v');
+};
+
+exports.paginate = async (query, reqQuery, Model) => {
+  const page = reqQuery.page * 1 || 1;
+  const limit = reqQuery.limit * 1 || 100;
+  const skip = (page - 1) * limit;
+
+  if (reqQuery.page) {
+    const numDocs = await Model.countDocuments();
+    if (skip >= numDocs) throw new Error('This page does not exist');
+  }
+
+  return query.skip(skip).limit(limit);
+};
